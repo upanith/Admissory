fad618d08f9e75cf8c548742f281bb14050794fe
Dr. Upanith S. Liyanaarachchi <upanith@gmail.com>
2021-06-20 20:36:56 +0530

    Basic Splash Screen added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap("C:/Users/upani/Desktop/Admissory.png"));
+    splash->show();
+
     MainWindow w;
-    w.show();
+    QTimer::singleShot(5000,splash,SLOT(close()));
+    QTimer::singleShot(5000,&w,SLOT(show()));
+
+    //w.show();
     return a.exec();
 }
